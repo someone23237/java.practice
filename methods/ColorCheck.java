d51055f24eee28ac2a305efce8c55363c94edc69
Someone23237 <sudeepthecoolest@gmail.com>
2026-05-28 19:32:10 -0700

    color check

---
+++ methods/ColorCheck.java
@@ -0,0 +1,15 @@
+package methods;
+
+public class ColorCheck {
+    public static void main(String[] args) {
+        String color = "blue";
+
+        if (color.equals("red")) {
+            System.out.println("Red is a warm color!");
+        } else if (color.equals("blue")) {
+            System.out.println("Blue is a cool color!");
+        } else {
+            System.out.println("That is a nice color!");
+        }
+    }
+}
