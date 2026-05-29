ad5aee78aefbc1bbb1cec67dada8242a2ebc27c0
Someone23237 <sudeepthecoolest@gmail.com>
2026-05-28 19:25:54 -0700

    age check

---
+++ methods/AgeCheck.java
@@ -0,0 +1,13 @@
+package methods;
+
+public class AgeCheck {
+    public static void main(String[] args) {
+        int age = 10;
+
+        if (age < 13) {
+            System.out.println("You are a kid!");
+        } else {
+            System.out.println("You are a teenager or older!");
+        }
+    }
+}
